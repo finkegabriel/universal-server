24ad5d48dae2cd46ddb5bf6e722dc2230682055a
Gabe <finkegabriel@gmail.com>
2020-04-27 12:56:35 -0700

    added

--- server.sh
+++ server.sh
@@ -4,16 +4,17 @@
     # try
     while :
     do
-    command= read -s -n 4 key
-    array+=($key)
-    if [[ $key = "" ]]; then 
-    echo 'You pressed enter!'
-    unset array
-    echo "${#array[@]}"
-    else
-    echo "You pressed '$key'"
-    echo "${#array[@]}"
-    echo $array
+        read -s -n 4 key
+        array+=($key)
+        if [[ $key = "" ]]; then 
+            echo 'You pressed enter!'
+            echo "${array[*]}"
+            unset array
+            echo "${#array[@]}"
+        else
+            echo "You pressed '$key'"
+            echo "${#array[@]}"
+            echo $array
     fi
     done
     #output
